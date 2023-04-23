2edf5c71af65628f41b5e77d8adc334a466eef11
Joshua George <horheyinc8@gmail.com>
2023-04-24 00:18:29 +0200

    fald

--- db/index.js
+++ db/index.js
@@ -7,7 +7,8 @@ require("dotenv").config();
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = `mongodb+srv://Joshua:zYsrDqabkCsDBVio@spotify-app.sz14qrx.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URI = `
+mongodb+srv://josh-ify-main-db-07347723f6c:GfFs45TAsJJ3DnY1AQffcJgqfd75EV@prod-us-central1-2.ih9la.mongodb.net/josh-ify-main-db-07347723f6c`;
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
@@ -16,5 +17,5 @@ mongoose
     );
   })
   .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
+    console.error("Error connecting to mongo and here is the error: ", err);
   });
